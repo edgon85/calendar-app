3579cea6b1ae57744735d333bcb989935083b6e3
Edwin <edgon85@gmail.com>
2022-08-16 10:58:58 -0600

    React big calendar implementado

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -1,2 +1,3 @@
 export * from './pages/CalendarPage';
 export * from './pages/components/Navbar';
+export * from './pages/components/CalendarEvent';
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,75 @@
-import { Navbar } from "../"
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { addHours } from 'date-fns';
+
+import { Navbar, CalendarEvent } from "../"
+import { localizer, getMessagesES } from '../../helpers';
+import { useState } from 'react';
 
 export const CalendarPage = () => {
+
+  const [lastView, setlastView] = useState( localStorage.getItem('lastView') || 'week' );
+
+  const events = [{
+    title: 'Pagar impuesos',
+    notes: 'pagar antes de 30 de cada mes',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'Camargo'
+    }
+  }];
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: '0.8',
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+  }
+
+  const onDoubleClick = ( event ) => {
+    console.log({doubleClick: event});
+  }
+
+  const onSelect = (event) => {
+    console.log({ click: event })
+  }
+
+  const onViewChanged = ( event ) => {
+    localStorage.setItem('lastView', event);
+    setlastView(event);
+  }
+
   return (
     <>
       <Navbar />
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        defaultView={lastView }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 3.5rem)' }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent
+        }}
+        onDoubleClickEvent={onDoubleClick }
+        onSelectEvent={ onSelect }
+        onView={ onViewChanged }
+      />
     </>
   )
 }
